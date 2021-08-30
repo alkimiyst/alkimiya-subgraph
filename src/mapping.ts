55f95fef95c9cefce8e0ccd5d90f686de3d02301
alkimist <alkimist>
2021-08-30 20:29:40 +0200

    update oracle

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,7 +6,7 @@ export function handleMinerStatusUpdated(event: MinerStatsUpdated): void {
   let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
   let status = new MinerStatus(id);
   status.sands = event.params.sands.toHexString();
-  status.reward = event.params.reward;
+  status.due = event.params.due;
   status.balance = event.params.balance;
   status.updated = event.params.updated;
   status.minerHashrate = event.params.minerHashrate;
