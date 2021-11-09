2a88e74ff9a9753d2a27dc8cf5fa83d97e85a5c1
Diego Antonioli Garcia <diego@alkimiya.io>
2021-11-09 09:47:19 -0300

    new event handler

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,15 +1,31 @@
-import { SandsRepo, OracleUpdate } from "../generated/schema";
+import { SandsRepo, ETHOracleUpdate, WBTCOracleUpdate } from "../generated/schema";
 // import { NewHashSandContract } from "../generated/HashSand/HashSandFactoryLib";
-import { OracleUpdate as NewOracleUpdate } from "../generated/Oracle/Oracle";
+import { OracleUpdate as NewETHOracleUpdate } from "../generated/ETHOracle/Oracle";
+import { OracleUpdate as NewWBTCOracleUpdate } from "../generated/WBTCOracle/Oracle";
 import { NewHashRepo as NewSandsRepo } from "../generated/SandsRepo/SandsRepoFactory";
 
-export function handleOracleUpdate(event: NewOracleUpdate): void {
-  let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
-  let status = new OracleUpdate(id);
+export function handleETHOracleUpdate(event: NewETHOracleUpdate): void {
+  // let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+  let status = new ETHOracleUpdate(event.params.referenceDay.toString());
   status.referenceDay = event.params.referenceDay;
   status.referenceBlock = event.params.referenceBlock;
   status.hashrate = event.params.hashrate;
   status.reward = event.params.reward;
+  status.fees = event.params.fees;
+  status.difficulty = event.params.difficulty;
+  status.timestamp = event.params.timestamp;
+  status.save();
+}
+
+export function handleWBTCOracleUpdate(event: NewWBTCOracleUpdate): void {
+  // let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+  let status = new WBTCOracleUpdate(event.params.referenceDay.toString());
+  status.referenceDay = event.params.referenceDay;
+  status.referenceBlock = event.params.referenceBlock;
+  status.hashrate = event.params.hashrate;
+  status.reward = event.params.reward;
+  status.fees = event.params.fees;
+  status.difficulty = event.params.difficulty;
   status.timestamp = event.params.timestamp;
   status.save();
 }
